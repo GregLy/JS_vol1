20cd50b4d8f07f2f6153b81513229f4b4a2d10f1
tstabla <t.stabla@hotmail.com>
2017-10-26 17:39:47 +0200

    js fix

--- zadanie8.5/assets/js/scripts.js
+++ zadanie8.5/assets/js/scripts.js
@@ -4,4 +4,6 @@
 		var triangleArea = (a*h/2);
 		console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 		var resultEl = document.getElementById('result');
-		
+
+
+		resultEl.innerHTML = '<p>' + triangleArea + '</p>';
