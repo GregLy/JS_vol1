e56873455e63362c85ae076b26601e8d2c1a1a51
GregLy <grze.lyszcz@gmail.com>
2017-10-26 17:19:43 +0200

    update

---
+++ zadanie8.4/assets/js/scripts.js
@@ -0,0 +1,10 @@
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+/*
+  block comment
+  console.log('The commented will not be called');
+*/
+
+// linear comment
