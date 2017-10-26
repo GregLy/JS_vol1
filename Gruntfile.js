d91ec8c2c6048bd97d12b30de66df782b1db2c19
GregLy <grze.lyszcz@gmail.com>
2017-10-26 16:57:38 +0200

    update

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'assets/css/style.css': 'assets/sass/main.scss'
+        }
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'assets/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'assets/images/build/'
+        }]
+      }
+    },
+    watch: {
+      styles: {
+        files: ['assets/sass/main.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        },
+      }
+    }
+  });
+	module.exports = function(grunt) {
+
+	grunt.initConfig({
+		jshint: {
+			all: ['js/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+  };
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+};
