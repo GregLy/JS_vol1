9630ef16d059ed517230df02a967ff1e9da489a9
GregLy <grze.lyszcz@gmail.com>
2017-11-01 19:45:50 +0100

    update

--- zadanie9.1/assets/js/scripts.js
+++ zadanie9.1/assets/js/scripts.js
@@ -2,13 +2,11 @@ const a = 6;
 const h = 6;
 
 function getTriangleArea( a, h ) {
-  if ( ( a  >= 0 ) || ( h >= 0 ) ) {
-    console.log ( 'result higher then zero' );
-  }   else if ( ( a  <= 0 ) && ( h <= 0) ) {
+  if ( ( a  >= 0 ) && ( h >= 0 ) ) {
+    const result = ( a * h / 2 );
+    return result;
+  } else if ( ( a  <= 0 ) && ( h <= 0 ) ) {
     console.log ( 'wrong data' );
-  }
-  function returnedValue {
-  return ( a*h/2 );
   }
 }
 
