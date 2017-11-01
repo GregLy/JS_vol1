0243063a77cda845c1967262d4cbdf93aab102e5
GregLy <grze.lyszcz@gmail.com>
2017-11-01 20:20:11 +0100

    update

--- zadanie9.1/assets/js/scripts.js
+++ zadanie9.1/assets/js/scripts.js
@@ -1,7 +1,6 @@
 const a = 6;
 const h = 6;
-
-function getTriangleArea( a, h ) {
+function getTriangleArea() {
   if ( ( a  >= 0 ) && ( h >= 0 ) ) {
     const result = ( a * h / 2 );
     return result;
@@ -10,3 +9,8 @@ function getTriangleArea( a, h ) {
   }
 }
 
+console.log( getTriangleArea( 10, 6 ) );
+
+const triangle1Area = getTriangleArea( 10, 15 );
+const triangle2Area = getTriangleArea( 5, 10 );
+const triangle3Area = getTriangleArea( 15, 25 );
