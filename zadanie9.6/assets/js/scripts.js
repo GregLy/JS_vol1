2bea45977ad934ef29d7e24625f5f11263200d21
GregLy <grze.lyszcz@gmail.com>
2017-11-04 15:12:41 +0100

    update

---
+++ zadanie9.6/assets/js/scripts.js
@@ -0,0 +1,8 @@
+let list = document.getElementById( 'list' );
+add.addEventListener( 'click', () => {
+  let element = document.createElement( 'li' );
+  element.innerHTML = 'item'; 
+  let li = document.getElementsByTagName( 'li' );
+  console.log( li.length );
+  list.appendChild( element );
+} );
