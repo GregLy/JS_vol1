52caba1d874a4451dda59603ea1087a86c9b4491
GregLy <grze.lyszcz@gmail.com>
2017-11-04 13:19:34 +0100

    update

--- zadanie9.5/assets/js/scripts.js
+++ zadanie9.5/assets/js/scripts.js
@@ -2,6 +2,6 @@ let btn = document.getElementsByClassName( 'button' );
 console.log( btn.length );
 
 for ( let btn = 0 ; btn < btn.length ; btn++ ){
-  alert( btn );
+  alert ( btn[ i ].innerText );
 }
 
