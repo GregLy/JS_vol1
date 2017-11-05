e41907042c50cd0cf8ecb4fb1268c6dea56d1832
GregLy <grze.lyszcz@gmail.com>
2017-11-05 13:40:56 +0100

    update

--- zadanie9.4/assets/js/scripts.js
+++ zadanie9.4/assets/js/scripts.js
@@ -1,9 +1,9 @@
 function drawTree( repeatItems ) {
   for ( let i = 0 ; i < repeatItems ; i++ ) {
     let star = '';
-    for ( let j = 0 ; j < i ; j++ ) {
-      star +='*';
-      console.log(star);
-      }
+    for ( let j = 0 ; j <= i ; j++ ) {
+      star += '*';
+    }
   }
+  console.log( star );
 }
