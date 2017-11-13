07401efc115fa0426af697613bd0304c98166d32
GregLy <grze.lyszcz@gmail.com>
2017-11-13 18:13:24 +0100

    update

--- zadanie10.3/assets/js/scripts.js
+++ zadanie10.3/assets/js/scripts.js
@@ -2,11 +2,8 @@ $(function(){
   let carouselList = $("#carousel ul");
   setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
   carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-  var firstItem = carouselList.find("li:first");
-  var lastItem = carouselList.find("li:last");
-});
-
-$(function(){
+  let firstItem = carouselList.find("li:first");
+  let lastItem = carouselList.find("li:last");
   lastItem.after(firstItem)
   carouselList.css({marginLeft:0}
 });
