f6f6bd20c47c36c8fad7a7ffde77d2d07a5a97ee
GregLy <grze.lyszcz@gmail.com>
2017-11-18 11:00:57 +0100

    update

--- zadanie11.5/assets/js/scripts.js
+++ zadanie11.5/assets/js/scripts.js
@@ -2,19 +2,21 @@ $( () => {
   function Button(text) {
     this.text = text || 'Hello';
   }
-
-  create: function() {
-    var self = this;
-    this.$element = $('<button>');
-    this.$element.text(this.text);
-    this.$element.click(function() {
-      alert(self.text);
-      $('body').append(this.$element);
-    });
+  
+  Button.prototype = {
+    create: function() {
+      var self = this;
+      this.$element = $('<button>');
+      this.$element.text(this.text);
+      this.$element.click(function() {
+        alert(self.text);
+        $('body').append(this.$element);
+      });
+    }
+    }
   }
 
   let btn1 = new Button('Hello!');
   btn1.create();
 
 } );
-
